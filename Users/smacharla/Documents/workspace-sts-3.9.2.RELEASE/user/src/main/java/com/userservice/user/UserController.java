e7a47257cb73275b208d924ff395563b61865c71
smacharla <theja141@gmail.com>
2018-06-06 22:45:07 +0530

    added only required files

---
+++ Users/smacharla/Documents/workspace-sts-3.9.2.RELEASE/user/src/main/java/com/userservice/user/UserController.java
@@ -0,0 +1,15 @@
+//package com.userservice.user;
+//
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class UserController {
+//
+//	@RequestMapping(value="api/users/{userName}")
+//	public String getUserName(@PathVariable String userName)
+//	{
+//		return "Hello"+userName;
+//	}
+//}
